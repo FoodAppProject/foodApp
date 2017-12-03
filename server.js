552cbc9f9fa1556252e633328b8b091cf33ddb12
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-12-03 10:59:31 -0800

    start of passport, update to server file

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var passport = require('passport')
+
+var PORT = process.env.PORT || 8080
+var db = require('./app/models/index')
+
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true}))
+
+require('./app/config/passport')(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./app/routes/html-routes.js')(app)
+require('./app/routes/api-routes.js')(app, passport)
+
+app.listen(PORT, function(){
+	console.log('Listening on port: ' + PORT)
+})
+
+module.exports = app
